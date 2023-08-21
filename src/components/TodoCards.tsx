3e9c31dfab344517cc237cae05dcdad84103b8ef
Denis <Ori-wiki@gmail.com>
2023-08-21 11:25:36 +0300

    export board

--- src/components/TodoCards.tsx
+++ src/components/TodoCards.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoCard from './TodoCard';
 import ExportDropdown from './ExportDropdown';
 import { useAppSelector } from '../hooks/reduxHooks';
 import Preloader from './Preloader/Preloader';
+
+import { authTrello, createNewBoard } from '../api/trelloapi';
 
 type SelectedCardProps = {
   id: string;
@@ -16,6 +18,7 @@ const TodoCards = () => {
   const gptPrompt = useAppSelector((state) => state.prompt.gptPrompt);
   const status = useAppSelector((state) => state.prompt.status);
   const [checkboxStates, setCheckboxStates] = useState<{ [id: string]: boolean }>({});
+  const [token, setToken] = useState('');
 
   const noParents = gptPrompt.filter((parent) => parent.parentId === null);
 
@@ -25,6 +28,26 @@ const TodoCards = () => {
       ...prevState,
       [id]: isChecked,
     }));
+  };
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const params = new URLSearchParams(url.hash.substr(1)); // Извлекаем строку после "#"
+
+    const token = params.get('token'); // Извлекаем параметр "token"
+
+    if (token) {
+      setToken(token); // Выводим токен в консоль
+    }
+  }, []);
+  const authClick = () => {
+    authTrello();
+  };
+  const exportClick = () => {
+    if (token) {
+      createNewBoard(token);
+    } else {
+      console.log('Нажмите 2 кнопку');
+    }
   };
 
   return (
@@ -49,7 +72,21 @@ const TodoCards = () => {
           </ul>
         )}
       </div>
-      <ExportDropdown gptAnswer={gptPrompt} modifiedTodoCards={selectedCards} />
+      <div className="flex mt-2 text-base">
+        <ExportDropdown gptAnswer={gptPrompt} modifiedTodoCards={selectedCards} />
+        <button
+          onClick={authClick}
+          className="inline-block w-52 mr-4 justify-between text-left bg-green p-3 hover:opacity-70 border border-gray rounded-lg"
+        >
+          Зарегаться в Trello
+        </button>
+        <button
+          onClick={exportClick}
+          className="inline-block w-52 mr-4 justify-between text-left bg-green p-3 hover:opacity-70 border border-gray rounded-lg"
+        >
+          Создать у себя доску
+        </button>
+      </div>
     </>
   );
 };
